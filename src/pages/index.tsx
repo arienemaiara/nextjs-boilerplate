a6dfc0a547fbe2947f5d51744071482b388d9ca2
Ariene Ribeiro <aribeiro@img.com.br>
2020-07-06 20:45:22 -0300

    First commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+const foo = 'bla';
+
+export default function Home({ title }: Props) {
   return (
     <div className="container">
       <Head>
