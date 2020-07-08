687b33f42be75b1a13d883c18bc49a177ac7a54f
Ariene Ribeiro <aribeiro@img.com.br>
2020-07-07 22:01:43 -0300

    Adding styled components to Main

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="favicon.ico" type="/img/icon-512.png" />
+        <link
+          rel="apple-touch-icon"
+          href="favicon.ico"
+          type="/img/icon-512.png"
+        />
+        <meta
+          name="description"
+          content="A simple projeto starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
