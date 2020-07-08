911ac66ffd1ee3afba54d3ce81e29cf61e00074a
Ariene Ribeiro <aribeiro@img.com.br>
2020-07-08 16:33:43 -0300

    Main storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,8 +1,16 @@
+import { text, withKnobs } from '@storybook/addon-knobs'
+
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  decorators: [withKnobs]
 }
 
-export const Basic = () => <Main />
+export const Basic = () => (
+  <Main
+    title={text('Title', 'React avançado')}
+    description={text('Description', 'Typescript, ReactJS')}
+  />
+)
